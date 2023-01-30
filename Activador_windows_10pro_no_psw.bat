c74ec270526ea3af13ed4feb62aa50b5308ccfc3
Phoevon <phoevonnukk@gmail.com>
2023-01-30 11:07:33 +0100

    Update Activador_windows_10pro_no_psw.bat

--- Activador_windows_10pro_no_psw.bat
+++ Activador_windows_10pro_no_psw.bat
@@ -4,5 +4,5 @@ set /p key=
 slmgr /ipk %key% && slmgr /skms kms.digiboy.ir && start /wait slmgr /ato
 
 echo Ya tienes Windows 10 activado, disfrutalo.
-CreateObject("WScript.Shell").PopUp "Cortesia de Alberto López",6
+CreateObject("WScript.Shell").PopUp "Cortesia de phoevon",6
 exit
